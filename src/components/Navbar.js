ea87b5aaedd322eec4b267fc4173305dfc713825
Atte Gartman <atte.gartman@gmail.com>
2018-01-21 20:43:51 +0200

    Added a services page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,23 @@ import React from 'react';
 import Link from 'gatsby-link';
 
 import github from '../img/github-icon.svg';
-import logo from '../img/logo.svg';
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+          <div className="navbar-title">Punainen tupa</div>
+
         </Link>
       </div>
       <div className="navbar-start">
+        <Link className="navbar-item" to="/services">
+          Services
+        </Link>
+
         <Link className="navbar-item" to="/about">
-          About
+          Approach
         </Link>
         <Link className="navbar-item" to="/products">
           Products
