9c1b92cff9411ef431936c66b047e46c3ce18eaf
agartman <atte.gartman@gmail.com>
2018-02-05 17:26:55 +0200

    Testing subscribe form

--- src/components/SubscribeRibbon.js
+++ src/components/SubscribeRibbon.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default ({ content, className }) => <div className="content ribbon">
   <h2 className="content-head content-head-ribbon is-center">Stay tuned via an occasional newsletter
 </h2>
-  <form name="subscribe" className="pure-form pure-form-stacked" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+  <form name="newsletter" className="pure-form pure-form-stacked" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
     <div className="pure-u-3-5">
-      <input className="pure-input-1" type="text" name="email" placeholder="E-mail" />
+      <input className="pure-input-1" type="text" id="email" name="email" placeholder="E-mail" />
     </div>
     <div className="pure-u-1-4">
       <button type="submit" className="pure-button pure-input-1">Subscribe</button>
