569f130452b3f9c07c3b9af4fa2a2e254152e2ee
agartman <atte.gartman@gmail.com>
2018-02-11 15:37:06 +0200

    Improved ux on the subscribe form

--- src/components/SubscribeRibbon.js
+++ src/components/SubscribeRibbon.js
@@ -9,17 +9,22 @@ export default class SubscribeRibbon extends React.Component {
   constructor(params) {
     super(params)
     this.state = {
+      title: "Stay tuned!",
       loading: false,
       loaded: false,
       email: '',
       buttonText: 'Subscribe'
+
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit = e => {
-    this.state.loading = true;
-    this.state.buttonText = "Subscribing...";
+    this.setState({
+      loading: true,
+      buttonText: "Subscribing..."
+    })
     e.preventDefault();
 
     fetch("/", {
@@ -31,17 +36,22 @@ export default class SubscribeRibbon extends React.Component {
       })
     })
       .then(() => {
-        this.state.loaded = true;
-        this.state.loading = false;
-        this.state.buttonText = "Thanks! I got ya! VIP list updated.";
+        this.setState({
+          loading: false,
+          loaded: true,
+          title: "Thanks! I got ya! VIP list updated.",
+          buttonText: "Done"
+        })
       })
       .catch(error => {
         console.log(error)
-        this.state.loaded = true;
-        this.state.loading = false;
-        this.state.buttonText = "Error occured.";
+        this.setState({
+          loading: false,
+          loaded: true,
+          buttonText: "Error occured, oops."
+        })
       }
-    );
+      );
 
   };
 
@@ -51,18 +61,18 @@ export default class SubscribeRibbon extends React.Component {
 
 
   render() {
-    const vars = this.state
     return (
       <div className="content ribbon">
-        <h2 className="content-head content-head-ribbon is-center">Stay tuned!</h2>
+        <h2 className="content-head content-head-ribbon is-center">{this.state.title}</h2>
         <form onSubmit={this.handleSubmit} className="pure-form align-center">
           <div className="pure-g">
-
+            {!this.state.loaded ? 
             <div className="pure-u-1 pure-u-lg-3-5">
-              <input className="pure-input-1" type="text" id="email" value={vars.email} onChange={this.handleChange} name="email" placeholder="E-mail for newsletter" />
-            </div>
+              <input className="pure-input-1" type="text" id="email" value={this.state.email} onChange={this.handleChange} name="email" placeholder="E-mail for newsletter" />
+            </div> : null}
+
             <div className="pure-u-1 pure-u-lg-2-5">
-              <button disabled={vars.loaded || vars.loading} type="submit" title={vars.buttonText} className="pure-button pure-input-1">{vars.buttonText}</button>
+              <button disabled={this.state.loaded || this.state.loading} type="submit" title={this.state.buttonText} className="pure-button pure-input-1">{this.state.buttonText}</button>
             </div>
 
           </div>
