403fc71b309d157b85fcd3145a9209576ca98b32
agartman <atte.gartman@gmail.com>
2018-02-11 15:12:47 +0200

    Removed axios, mods to form

--- src/components/SubscribeRibbon.js
+++ src/components/SubscribeRibbon.js
@@ -2,8 +2,8 @@ import React from 'react';
 export default class SubscribeRibbon extends React.Component {
   encode = (data) => {
     return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
   }
 
   constructor(params) {
@@ -20,18 +20,28 @@ export default class SubscribeRibbon extends React.Component {
 
   handleSubmit = e => {
     this.state.loading = true;
+    this.state.buttonText = "Subscribing...";
+    
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "newsletter", 
-      "email": this.state.email 
-    })
+      body: encode({
+        "form-name": "newsletter",
+        "email": this.state.email
+      })
     })
       .then(() => {
         this.state.loaded = true;
         this.state.loading = false;
+        this.state.buttonText = "Thanks! I got ya!";
       })
-      .catch(error => console.log(error));
+      .catch(error => {
+        console.log(error)
+        this.state.loaded = true;
+        this.state.loading = false;
+        this.state.buttonText = "Error occured.";
+      }
+    );
 
     e.preventDefault();
   };
@@ -46,7 +56,7 @@ export default class SubscribeRibbon extends React.Component {
     return (
       <div className="content ribbon">
         <h2 className="content-head content-head-ribbon is-center">Stay tuned!</h2>
-        <form name="newsletter" onSubmit={this.handleSubmit} className="pure-form align-center" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <form onSubmit={this.handleSubmit} className="pure-form align-center">
           <p className="hidden">
             <label>Don’t fill this out: <input name="bot-field" /></label>
           </p>
@@ -57,7 +67,7 @@ export default class SubscribeRibbon extends React.Component {
               <input className="pure-input-1" type="text" id="email" value={this.state.email} onChange={this.handleChange} name="email" placeholder="E-mail for newsletter" />
             </div>
             <div className="pure-u-1 pure-u-lg-2-5">
-              <button disabled={this.state.loaded} type="submit" title={this.state.buttonText} className="pure-button pure-input-1">{vars.buttonText}</button>
+              <button disabled={this.state.loaded || this.state.loading} type="submit" title={this.state.buttonText} className="pure-button pure-input-1">{vars.buttonText}</button>
             </div>
 
           </div>
