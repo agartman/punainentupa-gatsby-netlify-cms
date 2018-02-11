6cb003ca89a3ce74932699589df825f9fe2fabcc
agartman <atte.gartman@gmail.com>
2018-02-11 18:52:26 +0200

    Added a html.js with og metas

---
+++ src/html.js
@@ -0,0 +1,56 @@
+import React from "react"
+
+let stylesStr
+if (process.env.NODE_ENV === `production`) {
+  try {
+    stylesStr = require(`!raw-loader!../public/styles.css`)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+module.exports = class HTML extends React.Component {
+  render() {
+    let css
+    if (process.env.NODE_ENV === `production`) {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: stylesStr }}
+        />
+      )
+    }
+    return (
+      <html {...this.props.htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:locale" content="en_GB" />
+          <meta property="og:title"
+            content="Punainen Tupa Solutions Oy - Full stack web developer Atte Gartman" />
+          <meta property="og:description" content="Atte is a full stack developer in southern Finland area. Available for freelance work. Get in touch!" />
+          <meta property="og:image" content="https://punainentupa.fi/img/ogimage.png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="997" />
+        
+          {this.props.headComponents}
+          {css}
+        </head>
+        <body {...this.props.bodyAttributes}>
+          {this.props.preBodyComponents}
+          <div
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    )
+  }
+}
