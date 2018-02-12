d73d02af27a1f8768e9305ea13a55671f352b6fa
agartman <atte.gartman@gmail.com>
2018-02-12 18:59:50 +0200

    Highlighted the subhead a bit more, a bit more responsive on mobile as well.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,7 +7,7 @@ import ContactForm from './ContactForm'
 import Footer from './Footer'
 
 export default ({ content, className }) => <div className="container">
-    <Splash heading1="Hi! I'm Atte"  heading2="Let's get to work!" subheader="Full stack developer in Southern Finland area" />
+    <Splash heading1="Hi! I'm Atte"  heading2="Let's get to work!" subheader="Freelancer full stack developer in Southern Finland area" />
     <div className="content-wrapper">
         <SubscribeRibbon />
         <ContactForm />
